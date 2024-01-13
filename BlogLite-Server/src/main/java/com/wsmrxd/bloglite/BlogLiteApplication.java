481094ace6fe79f121d623974186d512b698239d
wsmrxd <wsmrxd@gmail.com>
2024-01-13 13:17:37 +0800

    移除遗留的OAuth2代码

--- BlogLite-Server/src/main/java/com/wsmrxd/bloglite/BlogLiteApplication.java
+++ BlogLite-Server/src/main/java/com/wsmrxd/bloglite/BlogLiteApplication.java
@@ -2,14 +2,7 @@ package com.wsmrxd.bloglite;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.client.ClientHttpRequestFactory;
-import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.web.client.RestTemplate;
-
-import java.net.InetSocketAddress;
-import java.net.Proxy;
 
 @SpringBootApplication
 @EnableScheduling
@@ -17,23 +10,6 @@ public class BlogLiteApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BlogLiteApplication.class, args);
-    }
-
-    @Bean
-    public RestTemplate restTemplate(ClientHttpRequestFactory factory) {
-        return new RestTemplate(factory);
-    }
-
-    @Bean
-    public ClientHttpRequestFactory simpleClientHttpRequestFactory() {
-        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
-        factory.setReadTimeout(15000);
-        factory.setConnectTimeout(15000);
-
-        // 本地测试时使用的Http代理
-        Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress("127.0.0.1", 524));
-//        factory.setProxy(proxy);
-        return factory;
     }
 
 }
